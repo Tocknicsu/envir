4ec4f507c02dc18ff1f7633c7c8022ff8acbd60d
Tocknicsu <wingemerald@gmail.com>
2015-06-01 21:48:25 +0800

    upate

---
+++ opencv.sh
@@ -0,0 +1,13 @@
+sudo apt-get install build-essential
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+mkdir ./opencv
+git clone https://github.com/Itseez/opencv.git ./opencv
+cd opencv
+mkdir ./release
+cd ./release
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j4
+sudo make install
+
