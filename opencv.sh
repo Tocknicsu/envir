b1374ca134178deda142c7ba142e8783cbfee216
Tocknicsu <wingemerald@gmail.com>
2015-06-02 00:36:08 +0800

    update

--- opencv.sh
+++ opencv.sh
@@ -2,6 +2,7 @@ sudo apt-get install build-essential
 sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
+rm -rf opencv
 mkdir ./opencv
 git clone https://github.com/Itseez/opencv.git ./opencv
 cd opencv
