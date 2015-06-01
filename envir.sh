a9bb33e2298d972747d7789ae11b0323c2ff138f
Tocknicsu <wingemerald@gmail.com>
2015-06-01 23:51:11 +0800

    update

--- envir.sh
+++ envir.sh
@@ -3,8 +3,8 @@
 sudo apt-get update
 sudo apt-get install -y zsh vim git curl build-essential cmake python-dev
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-wget https://raw.githubusercontent.com/Tocknicsu/vim/master/.vimrc -O ~/.vimrc
-wget https://raw.githubusercontent.com/Tocknicsu/vim/master/.ycm_extra_conf.py -O ~/.vim/.ycm_extra_conf.py
+wget https://raw.githubusercontent.com/Tocknicsu/envir/master/.vimrc -O ~/.vimrc
+wget https://raw.githubusercontent.com/Tocknicsu/envir/master/.ycm_extra_conf.py -O ~/.vim/.ycm_extra_conf.py
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-complete
