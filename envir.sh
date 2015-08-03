9ef2ea6691d7e74df8318641fd2a400615c5b472
Tocknicsu <wingemerald@gmail.com>
2015-08-03 15:19:33 +0800

    update

--- envir.sh
+++ envir.sh
@@ -1,7 +1,7 @@
 # zsh vim for ubuntu
 
 sudo apt-get update
-sudo apt-get install -y zsh vim git curl build-essential cmake python-dev
+sudo apt-get install -y zsh vim git curl build-essential cmake python-dev python3 python3-pip mysql-client libmysqlclient-dev htop iotop iftop
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 wget https://raw.githubusercontent.com/Tocknicsu/envir/master/.vimrc -O ~/.vimrc
 wget https://raw.githubusercontent.com/Tocknicsu/envir/master/.ycm_extra_conf.py -O ~/.vim/.ycm_extra_conf.py
