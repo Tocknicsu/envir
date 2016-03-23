89b12420702e6feadcb7da22839ed9d87ac35fbd
Tocknicsu <wingemerald@gmail.com>
2016-03-23 10:44:43 +0800

    update powerline install.sh

--- powerline-fonts.sh
+++ powerline-fonts.sh
@@ -1,5 +1,6 @@
-mkdir ~/.font
-cd ~/.font
-git clone https://github.com/Lokaltog/powerline-fonts.git 
-cd powerline-fonts/
-./install.sh
+sudo apt-get install python-pip git
+pip install --user git+git://github.com/Lokaltog/powerline
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
